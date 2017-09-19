2a6389336b55457c2760701436f62bbad3718c25
Denys Zhuravel <denys.zhuravel@albelli.com>
2017-09-19 16:26:39 +0200

    more for the windows build

--- make_magick.cmd
+++ make_magick.cmd
@@ -1,3 +1,5 @@
+@echo off
+
 SET magick_tag=%1
 SET magick_quantum=%2
 SET magick_hdri=%3
@@ -6,6 +8,18 @@ if "%1"=="" (
     echo "Please specify the Magick.NET tag to build"
     exit /b 1
 )
+
+if "%magick_quantum%"=="" (
+    echo "Setting magick_quantum to 8"
+    SET magick_quantum=8
+)
+
+
+if "%magick_hdri%"=="" (
+    echo "Setting HDRI to no"
+    SET magick_hdri="no"
+)
+
 
 rd /s /q artifacts
 
