459c77277a3d4e62e3ad8166ffbf9b64706626fd
Denys Zhuravel <denys.zhuravel@albelli.com>
2017-09-19 14:38:33 +0200

    comments, cleanup

--- build_internal.sh
+++ build_internal.sh
@@ -1,4 +1,3 @@
-
 # checkout the magick.net source based on tag
 
 git clone https://github.com/dlemstra/Magick.NET.git
@@ -6,25 +5,24 @@ cd Magick.NET
 git checkout tags/7.0.7.0
 git clean -fdx
 
+
+# use the script to checkout the corresponding native magick
 cd ImageMagick/Source
 ./Checkout.sh
 cd ImageMagick/ImageMagick/
 
-./configure --with-magick-plus-plus=no --with-quantum-depth=8 --enable-hdri=no --prefix=/tmp/imagemagick_built
+# build the native part
+./configure --with-magick-plus-plus=no --with-quantum-depth=8 --enable-hdri=no --prefix=/mnt/repo/artifacts/imagemagick_built
 
 make
 make install
 
+
+# build the wrapper
 cd ../../../../Source/Magick.NET.Native/
 cp /mnt/repo/CMakeList.txt $PWD
 cmake CMakeLists.txt
 make
-mkdir /tmp/libMagick.NET-x64.Native
+mkdir /mnt/repo/artifacts/libMagick.NET-x64.Native
 
-dotnet restore
-dotnet build
-
-cd ./src/Albelli.Viesus.Lambda/bin/Debug/netcoreapp1.0
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:pwd
-
-dotnet Albelli.Viesus.Lambda.dll
+cp ./libMagick.NET-Q8-x64.Native.so /mnt/repo/artifacts/libMagick.NET-x64.Native/
