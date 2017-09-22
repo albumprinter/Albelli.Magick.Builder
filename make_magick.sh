238f579d909c5344f8859b7bb85fff459746ef37
Denys Zhuravel <denys.zhuravel@albelli.com>
2017-09-22 11:45:26 +0200

    more generic build, still ugly

--- make_magick.sh
+++ make_magick.sh
@@ -16,3 +16,13 @@ docker build -t magick_builder_amzn .
 mkdir artifacts
 
 docker run -i --rm -v ${PWD}:/mnt/repo magick_builder_amzn /bin/sh -c "mkdir /tmp/build; cp /mnt/repo/build_internal.sh /tmp/build; cd /tmp/build; chmod +x build_internal.sh; ./build_internal.sh $magick_tag $magick_quantum $magick_hdri"
+
+mkdir empty
+
+cp nuget/template.nuspec Albelli.Magick.NET-Q"$magick_quantum"-x64.AmazonLinux.Natives.nuspec
+cp nuget/template.targets Albelli.Magick.NET-Q"$magick_quantum"-x64.AmazonLinux.Natives.targets
+
+sed -i -e "s/MAGICK_QUANTUM/$magick_quantum/g" ./Albelli.Magick.NET-Q"$magick_quantum"-x64.AmazonLinux.Natives.nuspec
+sed -i -e "s/MAGICK_VERSION/$magick_tag/g" ./Albelli.Magick.NET-Q"$magick_quantum"-x64.AmazonLinux.Natives.nuspec
+
+nuget pack
